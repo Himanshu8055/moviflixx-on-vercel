75402b8b829d773532a04f2d38623b5ffe0bdc26
Himanshu8055 <58818455+Himanshu8055@users.noreply.github.com>
2024-07-28 00:17:57 +0530

    "Remove flex and margin styles from .screenshotContainer, update img styles to set width to 80%."

---
+++ app/(navlink)/adult/page.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { PrismaClient } from '@prisma/client';
+import MovieCard from '../../components/movie_card';
+
+const prisma = new PrismaClient();
+
+const fetchMovies = async () => {
+  const posts = await prisma.adult.findMany({
+    where: {
+      type: 'Adult'
+    },
+    orderBy: {
+      release_date: 'desc'
+    },
+    take: 210,
+  });
+  await prisma.$disconnect();
+  return posts;
+};
+
+const Page = async () => {
+  const movies = await fetchMovies();
+
+  return (
+    <div className="m-2 flex flex-wrap justify-center">
+      {movies.map((movie: any) => (
+        <MovieCard key={movie.id} {...movie} />
+      ))}
+    </div>
+  );
+};
+
+export default Page;
