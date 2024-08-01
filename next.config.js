6f1420f443e8af6d641086f23d7146a375b0bebe
Himanshu8055 <58818455+Himanshu8055@users.noreply.github.com>
2024-08-01 15:42:13 +0530

    "Added 'imgbb.top' and 'postimages.org' to image domains in next.config.js"

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 // next.config.js
 module.exports = {
   images: {
-      domains: ['media.themoviedb.org'],
-      domains: ['upload.wikimedia.org'],
-    },
+    domains: ['media.themoviedb.org'],
+    domains: ['upload.wikimedia.org'],
+    domains: ['imgbb.top'],
+    domains: ['postimages.org'],
+  },
 }
